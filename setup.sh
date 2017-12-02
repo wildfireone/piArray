7c854470d57cf98873a2c374ac2176eeaaefd87a
decantr <theeasterlywood@gmail.com>
2017-12-02 19:38:30 +0000

    added npm to install list

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
+#!/bin/bash
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 nvm install 8.9.1
 sudo apt-get update
-sudo apt-get install -y python-minimal
-sudo apt-get install -y build-essential
+sudo apt-get install -y python-minimal build-essential npm
