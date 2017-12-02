34ab21cf699691ce488adde5706fb723354a2b6f
Luke Fisher <mail@lukefisher.me>
2017-12-02 12:50:22 +0000

    Update setup script to install nvm

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
-apt-get update
-apt-get install -y python-minimal
-apt-get install -y build-essential
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
+sudo apt-get update
+sudo apt-get install -y python-minimal
+sudo apt-get install -y build-essential
