24f0ab7ec0964b9d50ac463a8d9b8459322a4163
Wildfireone <wildfireone@gmail.com>
2017-12-02 14:13:00 +0000

    server test

---
+++ servertest.js
@@ -0,0 +1,27 @@
+const https = require('https');
+
+
+makeRequest('127.0.0.1', "type=red");
+makeRequest('127.0.0.1', "type=green");
+makeRequest('127.0.0.1', "type=blue");
+
+
+
+function makeRequest(ip, args){
+  https.get('https://'+ip+':3000?'+args, (resp) => {
+    let data = '';
+
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+      console.log('done to '+ip);
+    });
+
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+  });
+}
