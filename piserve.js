8e193e242bd3f5875ff43a9e22f94521c2ed910d
Wildfireone <wildfireone@gmail.com>
2017-12-02 13:29:26 +0000

    server test

--- piserve.js
+++ piserve.js
@@ -7,17 +7,54 @@ const assert = require('assert');
 const sense = require('sense-hat-led');
 const sleep = sense.sync.sleep;
 
+
+const B = [248, 252, 248];
+
+const on = [
+  B, B, B, B, B, B, B, B,
+  B, B, B, B, B, B, B, B,
+  B, B, B, B, B, B, B, B,
+  B, B, B, B, B, B, B, B,
+  B, B, B, B, B, B, B, B,
+  B, B, B, B, B, B, B, B,
+  B, B, B, B, B, B, B, B,
+  B, B, B, B, B, B, B, B,
+];
+
+const off = [
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+];
+
+
 const requestHandler = (request, response) => {
   console.log(request.url)
   var url_parts = url.parse(request.url, true);
   var query = url_parts.query;
-  let t = parseInt(query["t"]) ;
-  var lst = query["msg"];
-  console.log(query);
-  console.log(lst);
-  console.log(t);
-  sense.sync.showMessage(lst, t / 10);
-  sleep(t);
+  if(query["type"]=="msg"){
+    let t = parseInt(query["t"]) ;
+    var lst = query["msg"];
+    console.log(query);
+    console.log(lst);
+    console.log(t);
+    sense.sync.showMessage(lst, t / 10);
+    sleep(t);
+  }
+  else if(query["type"]=="on"){{
+    sense.sync.setPixels(on);
+  }
+  else{
+    sense.sync.setPixels(off);
+  }
+
+
+
 
 
   response.end('Shrapp pap pap!')
