6bbd2093cf69288635322b430087819d25202f9e
Luke Fisher <mail@lukefisher.me>
2017-12-03 01:01:33 +0000

    Tidy up server code to remove hardcoded arrays

--- piserve.js
+++ piserve.js
@@ -1,124 +1,68 @@
-// content of index.js
+'use strict';
+
 const http = require('http')
 const port = 3000
-var url = require('url');
-'use strict';
-const assert = require('assert');
+const url = require('url');
+
 const sense = require('sense-hat-led');
 const sleep = sense.sync.sleep;
 
+const buildArray = (size, value) => {
+  return Array(size).fill(value);
+}
 
 const B = [248, 252, 248];
 
-const on = [
-  B, B, B, B, B, B, B, B,
-  B, B, B, B, B, B, B, B,
-  B, B, B, B, B, B, B, B,
-  B, B, B, B, B, B, B, B,
-  B, B, B, B, B, B, B, B,
-  B, B, B, B, B, B, B, B,
-  B, B, B, B, B, B, B, B,
-  B, B, B, B, B, B, B, B,
-];
+const ledSize = 8 * 8;
 
-
-
-const off = [
-  0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0,
-  0, 0, 0, 0, 0, 0, 0, 0,
-];
-
+const on = buildArray(ledSize, B);
+const off = buildArray(ledSize, 0);
 
 const requestHandler = (request, response) => {
-  console.log(request.url)
+  console.log(request.url);
+
   var url_parts = url.parse(request.url, true);
   var query = url_parts.query;
+
   if (query["type"] == "msg") {
     let t = parseInt(query["t"]);
     var lst = query["msg"];
+
     console.log(query);
     console.log(lst);
     console.log(t);
+
     sense.sync.showMessage(lst, t / 10);
     sleep(t);
   } else if (query["type"] == "colour") {
     let r = parseInt(query["r"]);
     let g = parseInt(query["g"]);
     let b = parseInt(query["b"]);
-    var col = [r,g,b];
-    var localarray = [
-      col, col, col, col, col, col, col, col,
-      col, col, col, col, col, col, col, col,
-      col, col, col, col, col, col, col, col,
-      col, col, col, col, col, col, col, col,
-      col, col, col, col, col, col, col, col,
-      col, col, col, col, col, col, col, col,
-      col, col, col, col, col, col, col, col,
-      col, col, col, col, col, col, col, col,
-    ];
-sense.setPixels(localarray);
 
-  }
-  else if (query["type"] == "red") {
+    var colour = [r, g, b];
+    var localarray = buildArray(8 * 8, colour);
 
-    var col = [255,0,0];
-    var localarray = [
-      col, col, col, col, col, col, col, col,
-      col, col, col, col, col, col, col, col,
-      col, col, col, col, col, col, col, col,
-      col, col, col, col, col, col, col, col,
-      col, col, col, col, col, col, col, col,
-      col, col, col, col, col, col, col, col,
-      col, col, col, col, col, col, col, col,
-      col, col, col, col, col, col, col, col,
-    ];
-sense.setPixels(localarray);
+    sense.setPixels(localarray);
+  } else if (query["type"] == "red") {
+    var colour = [255, 0, 0];
+    var localarray = buildArray(ledSize, colour);
 
-  }
-  else if (query["type"] == "green") {
-    var col = [0,255,0];
-    var localarray = [
-      col, col, col, col, col, col, col, col,
-      col, col, col, col, col, col, col, col,
-      col, col, col, col, col, col, col, col,
-      col, col, col, col, col, col, col, col,
-      col, col, col, col, col, col, col, col,
-      col, col, col, col, col, col, col, col,
-      col, col, col, col, col, col, col, col,
-      col, col, col, col, col, col, col, col,
-    ];
-sense.setPixels(localarray);
+    sense.setPixels(localarray);
+  } else if (query["type"] == "green") {
+    var colour = [0, 255, 0];
+    var localarray = buildArray(ledSize, colour);
 
-  }
-  else if (query["type"] == "blue") {
-    var col = [0,0,255];
-    var localarray = [
-      col, col, col, col, col, col, col, col,
-      col, col, col, col, col, col, col, col,
-      col, col, col, col, col, col, col, col,
-      col, col, col, col, col, col, col, col,
-      col, col, col, col, col, col, col, col,
-      col, col, col, col, col, col, col, col,
-      col, col, col, col, col, col, col, col,
-      col, col, col, col, col, col, col, col,
-    ];
-sense.setPixels(localarray);
+    sense.setPixels(localarray);
+  } else if (query["type"] == "blue") {
+    var colour = [0, 0, 255];
+    var localarray = buildArray(ledSize, colour);
 
+    sense.setPixels(localarray);
   } else if (query["type"] == "on") {
     sense.sync.setPixels(on);
   } else {
     sense.sync.clear();
   }
-
-
-
-
 
   response.end('Shrapp pap pap!')
 }
